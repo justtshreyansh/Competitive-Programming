1dbda665555d853e9c4be7c2c25cd7a341c9ee56
shreyansh <shreyanshmarshal2@gmail.com>
2024-02-15 20:23:02 +0530

    Sasha and the beautiful array

---
+++ Contest/sashaAndTheBeautifulArray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        long long ans =0;
+        for(int i=1;i<n;i++){
+            ans+=(arr[i]-arr[i-1]);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
