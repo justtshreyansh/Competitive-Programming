ee232395044986e1f6bafb33521c16e6d32327ea
shreyansh <shreyanshmarshal2@gmail.com>
2024-02-17 17:39:44 +0530

    Atcoder contest:Print 341

---
+++ Contest/print341.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s = "";
+    for(int i=0;i<n;i++){
+        s+="10";
+    }
+    s+="1";
+    cout<<s<<endl;
+    return 0;
+}
